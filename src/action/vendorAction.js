c1d98f6fb83e738e33b6ad914e214dcf82f8bfbc
Elhamne <elham.ne94@gmail.com>
2023-07-24 01:59:04 +0330

    Component added for virtual list and data fetched

--- src/action/vendorAction.js
+++ src/action/vendorAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { VENDORS_REQUEST, VENDORS_SUCCESS, VENDORS_FAIL } from '../constants';
 
-export const fetchVendors = () => async (dispatch) => {
+export const fetchVendors = (page) => async (dispatch) => {
   try {
     dispatch({ type: VENDORS_REQUEST });
     const config = {
@@ -10,10 +10,10 @@ export const fetchVendors = () => async (dispatch) => {
       },
     };
     const { data } = await axios.get(
-      'https://snappfood.ir/mobile/v3/restaurant/vendors-list?lat=35.754&long=51.328&page-size=10&page=0',
+      `https://snappfood.ir/mobile/v3/restaurant/vendors-list?page=${page}&page-size=10&lat=35.754&long=51.328`,
       config
     );
-    dispatch({ type: VENDORS_SUCCESS, payload: data });
+    dispatch({ type: VENDORS_SUCCESS, payload: data.data.finalResult });
   } catch (e) {
     dispatch({
       type: VENDORS_FAIL,
--- src/components/Vendors.js
+++ src/components/Vendors.js
@@ -2,53 +2,98 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchVendors } from '../action/vendorAction';
+import UiVirtualScroll from './UiVirtualScroll';
 
 const Vendors = () => {
   const dispatch = useDispatch();
+  const buffer = 10 * 3;
+  const cache = buffer - 10;
+  const expressDelivery = 'ارسال اکسپرس';
+  const sellerDelivery = 'پیک فروشنده';
+
+  const [offset, setOffset] = useState(0);
   const [vendorsList, setVendorsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const vendors = useSelector((state) => state.vendors);
+  const loading = useSelector((state) => state.loading);
 
   useEffect(() => {
+    setIsLoading(loading);
     if (vendors) {
-      console.log('api response', vendors);
-      setVendorsList(vendors.data.finalResult);
+      setVendorsList(vendors.filter((vendor) => vendor.type === 'VENDOR'));
     }
-  }, [vendors]);
-
-  console.log(vendorsList);
+  }, [vendors, loading]);
 
   useEffect(() => {
-    dispatch(fetchVendors());
-  }, []);
+    dispatch(fetchVendors(offset));
+  }, [dispatch]);
+
+  const prevCallback = (newOffset) => {
+    dispatch(fetchVendors(newOffset));
+  };
+
+  const nextCallback = (newOffset) => {
+    dispatch(fetchVendors(newOffset));
+  };
 
   return (
     <>
       <Link to="/">Back to home</Link>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-cover">
-            <img src="" alt="cover" />
-          </div>
-          <div className="card-logo">
-            <img src="" alt="resturant-logo" />
-          </div>
-          <div className="card-body">
-            <div className="header">
-              <div className="title">تهیه غذای پارسا</div>
-              <div className="rate">۴.۸</div>
+
+      <UiVirtualScroll
+        buffer={buffer}
+        rowHeight={300}
+        height="100vh"
+        limit={10}
+        onPrevCallback={prevCallback}
+        onNextCallback={nextCallback}
+      >
+        <div className="card-container">
+          {vendorsList.map((vendor, index) => (
+            <div key={index} className="card">
+              {isLoading ? (
+                <>Loading...</>
+              ) : (
+                <>
+                  <div className="card-cover">
+                    <img src={vendor.data.backgroundImage} alt="cover" />
+                  </div>
+                  <div className="card-logo">
+                    <img src={vendor.data.logo} alt="resturant-logo" />
+                  </div>
+                  <div className="card-body">
+                    <div className="header">
+                      <div className="title">{vendor.data.title}</div>
+                      <div className="rate">
+                        {vendor.data.rate} {vendor.data.voteCount}
+                      </div>
+                    </div>
+                    <div className="desc">{vendor.data.description}</div>
+                    <div className="footer">
+                      <div className="delivery">
+                        <span>
+                          {vendor.data.badges[0]?.title
+                            ? expressDelivery
+                            : sellerDelivery}
+                        </span>
+                        <span>{vendor.data.deliveryFee} تومان</span>
+                      </div>
+                      {vendor.data.max_eta !== -1 ? (
+                        <div className="time">
+                          تا {vendor.data.max_eta} دقیقه
+                        </div>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
-            <div className="desc">ایرانی سنتی</div>
-            <div className="footer">
-              <div className="delivery">
-                <span>ارسال اکسپرس</span>
-                <span>۷۰۰۰ تومان</span>
-              </div>
-              <div className="time">تا ۳۵ دقیقه</div>
-            </div>
-          </div>
+          ))}
         </div>
-      </div>
+      </UiVirtualScroll>
     </>
   );
 };
